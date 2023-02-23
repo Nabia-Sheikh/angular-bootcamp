cd88ab1a2eee55c7edfb9cec2e051884700d247b
Nabia-Sheikh <nabiasheikh2222@gmail.com>
2023-02-23 23:26:43 +0500

    work started

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { TASKS } from '../../mockTasks';
-import { Task } from '../../Task';
+import { TASKS } from 'src/app/mockTasks';
+import { Task } from 'src/app/Task';
 
 @Component({
   selector: 'app-tasks',
@@ -9,4 +9,11 @@ import { Task } from '../../Task';
 })
 export class TasksComponent {
   tasks: Task[] = TASKS;
+
+  myTask : Task =  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  }
 }
