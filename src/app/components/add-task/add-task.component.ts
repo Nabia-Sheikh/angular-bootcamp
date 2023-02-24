e73dbd97604401da9d408a89170ad37e930ae5f4
Nabia-Sheikh <nabiasheikh2222@gmail.com>
2023-02-24 16:49:58 +0500

    add button toggle

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,15 +1,25 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.css']
+  styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent {
   @Output() onAddTask = new EventEmitter();
   text!: string;
   day!: string;
   reminder!: boolean;
+  showAddTask!: boolean;
+  subscription!: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   onSubmit() {
     if (!this.text && !this.day) {
@@ -20,11 +30,11 @@ export class AddTaskComponent {
     const newTask = {
       text: this.text,
       day: this.day,
-      reminder: this.reminder
-    }
+      reminder: this.reminder,
+    };
 
     this.onAddTask.emit(newTask);
-    
+
     this.text = '';
     this.day = '';
     this.reminder = false;
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   title: string = 'Task Tracker';
-  
+  showAddTask: boolean = false;
+  subscription!: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
   toggleBtn() {
-    console.log('toggle');
+    this.uiService.toggleAddTask();
   }
 }
